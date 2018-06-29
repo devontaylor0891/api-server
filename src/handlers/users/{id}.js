dd63da9fe0b27bf1a68265e2eccc12798fd80b1e
Devon <devontaylor0891@gmail.com>
2018-06-29 15:12:27 -0600

    Add user/id and product/id endpoints

--- src/handlers/users/{id}.js
+++ src/handlers/users/{id}.js
@@ -4,33 +4,40 @@ var connection = require('../../../db');
 module.exports = {
   get_users_id: function(req, res) {
     var userId = req.params.id;
-    connection.query(`
-      SELECT u.*, p.id AS producer_id
-      FROM users AS u
-      LEFT JOIN producers p
-        on p.user_id = u.id
-        WHERE u.id = ${userId}`,
-      function (error, userResult) {
-      if (userResult.length === 0) {
-        res.status(404).send({ message: "User not found"});
-        return;
-      }
-      console.log("userResult 1", userResult);
 
-      connection.query(`SELECT * FROM orders WHERE user_id = ${userId}`, function (error, ordersResults) {
-        var user = userResult.map(function(row) {
-          return {
-            id: row.id,
-            firstName: row.first_name,
-            email: row.email,
-            registrationDate: row.registration_date,
-            role: row.producer_id ? 'producer' : 'consumer',
-            orders: ordersResults || [],
-          }
-        });
-        res.status(200).send(user);
-      });
-    });
+    connection.query(
+      `SELECT * FROM users
+      WHERE id = ${userId}`, function (error, results) {
+        return res.status(200).send(results);
+      }
+    )
+    // connection.query(`
+    //   SELECT u.*, p.id AS producer_id
+    //   FROM users AS u
+    //   LEFT JOIN producers p
+    //     on p.user_id = u.id
+    //     WHERE u.id = ${userId}`,
+    //   function (error, userResult) {
+    //   if (userResult.length === 0) {
+    //     res.status(404).send({ message: "User not found"});
+    //     return;
+    //   }
+    //   console.log("userResult 1", userResult);
+
+    //   connection.query(`SELECT * FROM orders WHERE user_id = ${userId}`, function (error, ordersResults) {
+    //     var user = userResult.map(function(row) {
+    //       return {
+    //         id: row.id,
+    //         firstName: row.first_name,
+    //         email: row.email,
+    //         registrationDate: row.registration_date,
+    //         role: row.producer_id ? 'producer' : 'consumer',
+    //         orders: ordersResults || [],
+    //       }
+    //     });
+    //     res.status(200).send(user);
+    //   });
+    // });
   },
 };
   
