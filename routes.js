f5129fffebe9d43a885d319bfed31312c4c3ffd6
Devon <devontaylor0891@gmail.com>
2018-06-30 17:09:52 -0600

    Returning new user after auth0 signup.

--- routes.js
+++ routes.js
@@ -8,10 +8,10 @@ var express = require('express');
 
 var router = express.Router();
 
-router.route('/users').get(users.get_users);
-router.route('/users').post(users.post_users);
-router.route('/users/:id').get(users.get_users_id);
-router.route('/users/userId/:userId').get(users.get_users_userId);
+router.route('/users').get(users.get_users); // GET all users
+router.route('/users').post(users.post_users); // CREATE a new user
+router.route('/users/:id').get(users.get_users_id); // GET a single user
+router.route('/users/:id').patch(users.patch_users_id); // UPDATE a user
 router.route('/users/:id/orders').get(users.get_users_id_orders);
 // BODY: array of products (object with id and qty)
 router.route('/users/:id/orders').post(users.post_users_id_orders);
