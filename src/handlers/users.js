b2311b5dd3e388e224b6319083c5a5507ba4187d
Devon <devontaylor0891@gmail.com>
2018-07-01 12:25:52 -0600

    Returning new user after auth0 signup.

--- src/handlers/users.js
+++ src/handlers/users.js
@@ -82,7 +82,7 @@ module.exports = {
       //   `
       // ),
       'SET SQL_SAFE_UPDATES=0; UPDATE users SET first_name = ?, email = ?, role = ? WHERE id = ?; SET SQL_SAFE_UPDATES=1;',
-      ['${req.body.firstName}', '${req.body.email}', '${req.body.role}', '${req.params.id}'],
+      ['${req.body.firstName}', '${req.body.email}', '${req.body.role}', req.params.id],
       function (err, result) {
         if (err) {
           console.log('error in update user:', err);
@@ -90,7 +90,7 @@ module.exports = {
         } else {
           console.log('user updated: ', result);
           console.log('req body: ', req.body);
-          console.log('params.id: ', '${req.params.id}');
+          console.log('params.id: ', req.params.id);
           return res.status(200).send(result);
         }
         
