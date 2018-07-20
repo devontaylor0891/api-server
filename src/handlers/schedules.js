90a817cb6557bda1edaf1ff5c20706dffab2c4db
Devon <devontaylor0891@gmail.com>
2018-07-20 12:25:35 -0600

    Format get schedules

--- src/handlers/schedules.js
+++ src/handlers/schedules.js
@@ -8,7 +8,27 @@ module.exports = {
   get_schedules: function(req, res) {
     connection.query(
       `SELECT * FROM schedules`, function (error, schedulesResult) {
-        return res.status(200).send(schedulesResult);
+        let schedules = schedulesResult.map(function(row) {
+          return {
+            id: row.schedule_id,
+            producerId: row.producer_id_fk_s,
+            type: row.schedule_type,
+            description: row.description,
+            startDateTime: row.start_date_time,
+            endDateTime: row.end_date_time,
+            hasFee: row.has_fee,
+            hasWiaver: row.has_waiver,
+            latitude: row.latitude,
+            longitude: row.longitude,
+            city: row.city,
+            province: row.province,
+            orderDeadline: row.order_deadline,
+            address: row.address,
+            fee: row.fee,
+            feeWaiver: row.fee_waiver
+          }
+        });
+        return res.status(200).send(schedules);
       }
     )
   },
