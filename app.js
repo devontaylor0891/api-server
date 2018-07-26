2d136f0c1f39ff5754e3966f9cb90311440c1644
devontaylor0891 <devontaylor0891@gmail.com>
2018-07-25 20:25:27 -0600

    Small changes to routes.js.

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var hike = require('./routes/hike');
+// var indexRouter = require('./routes/index');
+// var usersRouter = require('./routes/users');
+// var hike = require('./routes/hike');
 var routes = require('./routes');
 
 var app = express();
@@ -20,8 +20,8 @@ app.use('/api', routes);
 // app.post('/add_hike', hike.add_hike);
 
 // // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
 
 // app.use(logger('dev'));
 // app.use(express.json());
