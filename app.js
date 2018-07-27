c98e33cda4dc98d644678577c0a7ee15927822e5
Devon <devontaylor0891@gmail.com>
2018-07-27 16:04:51 -0600

    Working on put products

--- app.js
+++ app.js
@@ -14,12 +14,17 @@ var app = express();
 
 app.use(bodyParser.json({ type: 'application/json' }));
 
-app.use(function(req, res, next) {
+app.all('/', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
   next();
-});
+ });
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
 
 app.use('/api', routes);
 
