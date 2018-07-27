b5fba4d34b0e8124a1f3f3ee143c82d6aebf94b8
Devon <devontaylor0891@gmail.com>
2018-07-27 10:40:43 -0600

    Added typecasting for booleans

--- db.js
+++ db.js
@@ -9,12 +9,15 @@ var connection = mysql.createConnection({
   typeCast: function castField( field, useDefaultTypeCasting ) {
     // We only want to cast bit fields that have a single-bit in them. If the field
     // has more than one bit, then we cannot assume it is supposed to be a Boolean.
-    if ( ( field.type === "BIT" ) && ( field.length === 1 ) ) {
-        var bytes = field.buffer();
+    if ( ( field.type === "TINYINT" ) && ( field.length === 1 ) ) {
+      if (field.type == 'TINY' && field.length == 1) {
+        return (field.string() == '1'); // 1 = true, 0 = false
+      }
+        // var bytes = field.buffer();
         // A Buffer in Node represents a collection of 8-bit unsigned integers.
         // Therefore, our single "bit field" comes back as the bits '0000 0001',
         // which is equivalent to the number 1.
-        return( bytes[ 0 ] === 1 );
+        // return( bytes[ 0 ] === 1 );
     }
     return( useDefaultTypeCasting() );
   }
