d9b965f7cf426bf59b9edfb5fc5cd321cb78aad7
Devon <devontaylor0891@gmail.com>
2018-07-27 16:14:25 -0600

    Add cors package.

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var createError = require('http-errors');
 var express = require('express');
-var cors = require('cors');
+// var cors = require('cors');
 var bodyParser = require('body-parser');
 var path = require('path');
 var cookieParser = require('cookie-parser');
@@ -13,7 +13,15 @@ var routes = require('./routes');
 
 var app = express();
 
-app.options('*', cors());
+// app.options('*', cors());
+
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Credentials', true)
+  res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PATCH, PUT, DELETE')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  next()
+})
 
 app.use(bodyParser.json({ type: 'application/json' }));
 
