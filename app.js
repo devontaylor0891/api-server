ac15b58e84e505504494cdb7baa444fba97f2469
Devon <devontaylor0891@gmail.com>
2018-07-27 15:58:41 -0600

    Working on put products

--- app.js
+++ app.js
@@ -14,6 +14,12 @@ var app = express();
 
 app.use(bodyParser.json({ type: 'application/json' }));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use('/api', routes);
 
 // app.get('/hikes', hike.index);
