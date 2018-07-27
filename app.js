4d02e0686ed9b29473a05682869cec2283eca51d
Devon <devontaylor0891@gmail.com>
2018-07-27 16:35:22 -0600

    Add cors package.

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ var app = express();
 
 var allowCrossDomain = function(req, res, next) {
   if ('OPTIONS' == req.method) {
+    console.log('cors tried: ', req);
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
