2a9db6561240270e72f05054c71f3170cf9f7bb2
Devon <devontaylor0891@gmail.com>
2018-07-29 20:01:49 -0600

    Add cors package.

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ var app = express();
 app.options("/*", function(req, res, next){
   console.log('reaq: ', req);
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Method', 'GET,PUT,POST,PATCH,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,PATCH,DELETE,OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
   console.log('res received', res);
   res.send(200);
