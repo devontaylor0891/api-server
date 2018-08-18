129dfc6ac74654606562d6d376ec3e9ce465a216
Devon <devontaylor0891@gmail.com>
2018-08-18 09:13:55 -0600

    Add post new order endpoint.

--- src/handlers/orders.js
+++ src/handlers/orders.js
@@ -18,11 +18,10 @@ module.exports = {
     )
   },
   post_order: function (req, res) {
-    let postQuery = {
+    let orderPostQuery = {
       producer_id_fk_o: `${req.body.id}`,
       consumer_id_fk_o: `${req.body.id}`,
       schedule_id_fk_o: `${req.body.id}`,
-      product_quantities_id_fk_o: `${req.body.id}`,
       producer_comment: `${req.body.id}`,
       consumer_comment: `${req.body.id}`,
       delivery_address: `${req.body.id}`,
@@ -32,15 +31,40 @@ module.exports = {
       order_value: `${req.body.id}`,
       incomplete_reason: `${req.body.id}`
     };
-    console.log('post new order: ', postQuery);
+    let productQuantitiesPostQuery;
+    console.log('post new order: ', orderPostQuery);
     connection.query(
-      'INSERT INTO orders SET ?',
-      postQuery,
+      `INSERT INTO orders 
+      SET ?`,
+      orderPostQuery,
       function(err, result) {
         if (err) {
           res.send('error:', err);
-        } else {
-          return res.status(200).send(result);
+        } else { // success
+          // use result.insertId to add to other table
+          // using a for loop to add to product_quantities table
+          let productQuantities = req.body.orderDetails.productQuantities;
+          for (let i = 0; i < productQuantities.length; i++) {
+            // build query
+            productQuantitiesPostQuery = {
+              order_id_fk_pol: result.insertId,
+              product_id_fk_pok: `${productQuantities[i].productId}`,
+              quantity: `${productQuantities[i].orderQuantity}`
+            };
+            // run insert query
+            connection.query(
+              `INSERT INTO product_order_quantities
+              SET ?`,
+              productQuantitiesPostQuery,
+              function (err, result) {
+                if (err) {
+                  res.send('error: ', err);
+                } else {
+                  return res.status(200).send(result);
+                }
+              }
+            )
+          } 
         }
       }
     )
