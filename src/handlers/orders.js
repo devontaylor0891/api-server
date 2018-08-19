2c07094c2430dfee3b1eb1b96a6b913b0f4db675
Devon <devontaylor0891@gmail.com>
2018-08-19 14:42:08 -0600

    Adding joins to getOrders.

--- src/handlers/orders.js
+++ src/handlers/orders.js
@@ -19,7 +19,7 @@ module.exports = {
     //     return res.status(200).send(orders);
     //   }
     // )
-    var sqlString = 'SELECT * FROM orders LEFT JOIN producers ON orders.producer_id_fk_o = producers.producer_id';
+    var sqlString = 'SELECT * FROM orders LEFT JOIN producers ON orders.producer_id_fk_o = producers.producer_id LEFT JOIN users ON orders.consumer_id_fk_o = users.id LEFT JOIN product_order_quantities ON orders.order_id = product_order_quantities.order_id_fk_pok LEFT JOIN products ON product_order_quantities.product_id_fk_pok = products.product_id';
     var options = {sql: sqlString, nestTables: true, values: [1]};
   connection.query(
       // `SELECT *
@@ -36,13 +36,7 @@ module.exports = {
       function (error, ordersResult) {
         let orders = ordersResult.map(function (row) {
           return {
-            id: row.order_id,
-            producerId: row.producer_id_fk_o,
-            name: row.name,
-            firstName: row.first_name,
-            quantity: row.quantity,
-            pricePerUnit: row.pricePerUnit,
-            wholeObject: row
+            order: row
           }
         });
         console.log('orders: ', orders);
