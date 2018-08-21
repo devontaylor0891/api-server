e7e0a2da307e16dc95ed21e3a22a77ca144c5550
devontaylor0891 <devontaylor0891@gmail.com>
2018-08-20 18:17:41 -0600

    get orders endpoint.

--- src/handlers/orders.js
+++ src/handlers/orders.js
@@ -4,13 +4,70 @@ var connection = require('../../db');
 module.exports = {
   get_orders: function(req, res) {
 
-    connection.query('SELECT * FROM orders LEFT JOIN producers ON orders.producer_id_fk_o = producers.producer_id LEFT JOIN users ON orders.consumer_id_fk_o = users.id; SELECT * FROM product_order_quantities LEFT JOIN products ON product_order_quantities.product_id_fk_pok = products.product_id WHERE product_order_quantities.order_id_fk_pok = 15', function (error, results, fields) {
-      if (error) throw error;
-      // `results` is an array with one element for every statement in the query:
-      console.log('order: ', results[0]); // [{1: 1}]
-      console.log('product info: ', results[1]); // [{2: 2}]
-      return res.status(200).send(results);
-    });
+    // create an array to hold our completed results
+    let ordersArray;
+
+    // build a query inside a promise, this will return the rows from the query
+    function promisedQuery(sql) { 
+      return new Promise ((resolve, reject) => {
+        connection.query(sql, function(err, rows) {
+          if ( err ) {
+            return reject( err );
+          }
+          resolve( rows );
+        })
+      });
+    };
+
+    // build the queries
+    let getOrdersSql = 'SELECT * FROM orders LEFT JOIN producers ON orders.producer_id_fk_o = producers.producer_id LEFT JOIN users ON orders.consumer_id_fk_o = users.id;';
+    let getOrdersQueryOptions = {
+      sql: getOrdersSql,
+      nestTables: true
+    };
+    let getProductsSql = 'SELECT * FROM product_order_quantities LEFT JOIN products ON product_order_quantities.product_id_fk_pok = products.product_id WHERE product_order_quantities.order_id_fk_pok = ?';
+    let getProductsQueryOptions = {
+      sql: getProductsSql,
+      nestTables: true,
+      values: []
+    };
+    let ordersReceived, productsReceived, results;
+
+    // then call the promisedQuery and pass the queries into it
+    promisedQuery(getOrdersQueryOptions)
+      .then(rows => {
+        // do stuff with the returned rows from the first query
+        ordersReceived = rows;
+        console.log('orders received: ', ordersReceived.length);
+        // retreive the orderId and assign to the values in the getProductsQueryOptions
+        for (let i = 0; i < ordersReceived.length; i++) {
+          let orderId = ordersReceived[i].orders.order_id;
+          getProductsQueryOptions.values.push(orderId);
+        }
+        // call the promisedQuery again
+        return promisedQuery(getProductsQueryOptions)
+      })
+      .then(rows => {
+        // do stuff with the rows from that query
+        productsReceived = rows;
+        console.log('prodcuts received: ', productsReceived.length);
+      })
+      .then(rows => {
+        // build the final objects
+        ordersArray = rows;
+        // send the results
+        return res.status(200).send(ordersArray);
+      }).catch(err => {
+        return res.status(500).send(err);
+      });
+
+    // connection.query('SELECT * FROM orders LEFT JOIN producers ON orders.producer_id_fk_o = producers.producer_id LEFT JOIN users ON orders.consumer_id_fk_o = users.id; SELECT * FROM product_order_quantities LEFT JOIN products ON product_order_quantities.product_id_fk_pok = products.product_id WHERE product_order_quantities.order_id_fk_pok = 15', function (error, results, fields) {
+    //   if (error) throw error;
+    //   // `results` is an array with one element for every statement in the query:
+    //   console.log('order: ', results[0]); // [{1: 1}]
+    //   console.log('product info: ', results[1]); // [{2: 2}]
+    //   return res.status(200).send(results);
+    // });
 
 
     // connection.query(
