8c13fa7afae219528e5d9b478e9f972129ccf8dd
Devon <devontaylor0891@gmail.com>
2018-08-26 13:14:34 -0600

    Refactor userOrders/id endpoint.

--- src/handlers/schedules.js
+++ src/handlers/schedules.js
@@ -36,40 +36,39 @@ module.exports = {
   post_schedules: function (req, res) {
     // return res.send(201);
     console.log('new schedule body: ', req.body);
-    return res.send(201);
-    // let postQuery = {
-    //   user_id_fk_products: `${req.body.producer.id}`,
-    //   name: `${req.body.name}`,
-    //   description: `${req.body.description}`,
-    //   image: `${req.body.image}`,
-    //   pricePerUnit: `${req.body.pricePerUnit}`,
-    //   unit: `${req.body.unit}`,
-    //   unitsPer: `${req.body.unitsPer}`,
-    //   category: `${req.body.category}`,
-    //   subcategory: `${req.body.subcategory}`,
-    //   date_added: `${req.body.dateAdded}`,
-    //   qty_available: `${req.body.qtyAvailable}`,
-    //   qty_pending: 0,
-    //   qty_accepted: 0,
-    //   qty_completed: 0,
-    //   is_obsolete: `${req.body.isObsolete}`,
-    //   schedule_list: 0,
-    //   producer_id_fk_products: `${req.body.producerId}`
-    // };
-    // console.log('postQuery: ', postQuery);
-    // connection.query(
-    //   `INSERT INTO schedules SET ?`,
-    //   postQuery,
-    //   function(err, result) {
-    //     if (err) {
-    //       console.log('error in create sched:', err);
-    //       res.send('error:', err);
-    //     } else {
-    //       console.log('sched created: ', result);
-    //       return res.status(200).send(result);
-    //     }
-    //   }
-    // )
+    // return res.send(201);
+    let postQuery = {
+      producer_id_fk_s: `${req.body.producerId}`,
+      schedule_type: `${req.body.type}`,
+      description: `${req.body.description}`,
+      start_date_time: `${req.body.startDateTime}`,
+      end_date_time: `${req.body.endDateTime}`,
+      has_fee: `${req.body.hasFee}`,
+      has_waiver: `${req.body.hasWiaver}`,
+      latitude: `${req.body.latitude}`,
+      longitude: `${req.body.longitude}`,
+      city: `${req.body.city}`,
+      province: `${req.body.province}`,
+      order_deadline: `${req.body.orderDeadline}`,
+      address: `${req.body.address}`,
+      fee: `${req.body.fee}`,
+      fee_waiver: `${req.body.feeWaiver}`,
+      user_id_fk_schedules: `${req.body.userId}`
+    };
+    console.log('postQuery: ', postQuery);
+    connection.query(
+      `INSERT INTO schedules SET ?`,
+      postQuery,
+      function(err, result) {
+        if (err) {
+          console.log('error in create sched:', err);
+          res.send('error:', err);
+        } else {
+          console.log('sched created: ', result);
+          return res.status(200).send(result);
+        }
+      }
+    )
   },
 
   get_schedules_id: function(req, res) {
