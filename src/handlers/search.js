36efad6c695ef82c85158e604a9bbe29e5151d3d
devontaylor0891 <devontaylor0891@gmail.com>
2018-08-28 20:01:59 -0600

    Attempt at distance search.

--- src/handlers/search.js
+++ src/handlers/search.js
@@ -7,7 +7,7 @@ module.exports = {
     
     // get the search parameters
     // let distance = req.body.radius;
-    let distance = 250;
+    let distance = 25;
     // let latitude = req.body.lat;
     let latitude = 50.1436;
     // let longitude = req.body.long;
