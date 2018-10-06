8e830a6e35cc0dc2c9a2c6e128fdff017e7207e9
devontaylor0891 <devontaylor0891@gmail.com>
2018-10-05 20:24:11 -0600

    Fix search to return multiple producers' info.

--- src/handlers/search.js
+++ src/handlers/search.js
@@ -66,7 +66,7 @@ module.exports = {
       nestTables: true
     };
 
-    let getProducersSql = 'SELECT * FROM producers LEFT JOIN users ON producers.user_id = users.id WHERE producer_id IN (?)';
+    let getProducersSql = 'SELECT * FROM producers LEFT JOIN users ON producers.user_id = users.id WHERE producer_id IN (123123128,123123129)';
     let getProducersQueryOptions = {
       sql: getProducersSql,
       values: null,
