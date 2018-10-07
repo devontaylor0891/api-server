a34116440c1618638045e733e104ea26885b9f78
Devon <devontaylor0891@gmail.com>
2018-10-07 11:21:00 -0600

    Add delete product endpoint.

--- src/handlers/products/{id}.js
+++ src/handlers/products/{id}.js
@@ -110,7 +110,7 @@ module.exports = {
 
   delete_products_id: function (req, res) {
     console.log('delete products: ', req.body);
-    return res.status(200).send(req.body.id);
+    return res.sendStatus(200).send(req.body.id);
   }
 
 };
