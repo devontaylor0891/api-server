569418e2b1c86b49fed07bea6fa3628f0516bca4
Devon <devontaylor0891@gmail.com>
2018-11-09 09:51:44 -0600

    Changed presigned url endpoint to post.

--- src/handlers/images/images.js
+++ src/handlers/images/images.js
@@ -1,9 +1,9 @@
 'use strict';
 let AWS = require('aws-sdk');
 AWS.config.update({
-    accessKeyId: process.env.ACCESS_ID, 
-    secretAccessKey: process.env.ACCESS_SECRET,
-    region:'us-west-2'
+  accessKeyId: process.env.IMAGES_ACCESS_ID, 
+  secretAccessKey: process.env.IMAGES_ACCESS_SECRET,
+  region:'us-west-2'
 });
 
 module.exports = {
@@ -13,27 +13,20 @@ module.exports = {
 
     console.log('image url req.body: ', req.body);
 
-    // (imageName: any): Observable<string> {
-      AWS.config.update({
-        accessKeyId: ``,
-        secretAccessKey: ``,
-        region: 'us-west-2'
-      });
-      const s3 = new AWS.S3();
-      let params = {
-        Bucket: 'onlylocalfood-images',
-        // Key: req.body.imageName,
-        Key: 'example',
-        Expires: 1000000,
-        ContentType: 'image/jpeg'
-        // ACL: 'public-read'
-      };
-      let url = s3.getSignedUrl('putObject', params);
-      console.log('params: ', params);
-      console.log('url: ', url);
-      return res.status(200).send(url);
-    // }
-    // return res.status(200).send(req.params);
+    const s3 = new AWS.S3();
+    let params = {
+      Bucket: 'onlylocalfood-images',
+      // Key: req.body.imageName,
+      Key: 'example',
+      Expires: 1000000,
+      ContentType: 'image/jpeg'
+      // ACL: 'public-read'
+    };
+    let url = s3.getSignedUrl('putObject', params);
+    console.log('params: ', params);
+    console.log('url: ', url);
+    return res.status(200).send(url);
+
   }
 
 };
