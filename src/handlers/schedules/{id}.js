a33eff637ea58b3b5d62d6a2ec1a1addb73202a6
Devon <devontaylor0891@gmail.com>
2018-11-09 15:51:01 -0600

    Fix to booleans in put sched.

--- src/handlers/schedules/{id}.js
+++ src/handlers/schedules/{id}.js
@@ -35,40 +35,116 @@ module.exports = {
 
   put_schedules_id: function (req, res) {
     console.log('put schedule called: ', req.body);
-    let postQuery = {
-      producer_id_fk_s: `${req.body.producerId}`,
-      schedule_type: `${req.body.type}`,
-      description: `${req.body.description}`,
-      start_date_time: `${req.body.startDateTime}`,
-      end_date_time: `${req.body.endDateTime}`,
-      has_fee: `${req.body.hasFee}`,
-      has_waiver: `${req.body.hasWaiver}`,
-      latitude: `${req.body.latitude}`,
-      longitude: `${req.body.longitude}`,
-      city: `${req.body.city}`,
-      province: `${req.body.province}`,
-      order_deadline: `${req.body.orderDeadline}`,
-      address: `${req.body.address}`,
-      fee: `${req.body.fee}`,
-      fee_waiver: `${req.body.feeWaiver}`
-    };
-    console.log('postQuery: ', postQuery);
-    let scheduleId = req.params.id;
-    connection.query(
-      `UPDATE schedules 
-      SET ?
-      WHERE schedule_id = ?;`,
-      [postQuery, scheduleId],
-      function (err, result) {
-        if (err) {
-          console.log('error in update sched:', err);
-          return res.status(500).send('error:', err);
-        } else {
-          console.log('sched updated: ', result);
-          return res.status(200).send(result);
-        }
-      } 
-    )
+    // get has fee and has waiver values
+    let hasFeeVal = req.body.hasFee;
+    let hasWaiverVal = req.body.hasWaiver;
+    if (!hasFeeVal) {
+      let postQuery = {
+        producer_id_fk_s: `${req.body.producerId}`,
+        schedule_type: `${req.body.type}`,
+        description: `${req.body.description}`,
+        start_date_time: `${req.body.startDateTime}`,
+        end_date_time: `${req.body.endDateTime}`,
+        has_fee: false,
+        has_waiver: false,
+        latitude: `${req.body.latitude}`,
+        longitude: `${req.body.longitude}`,
+        city: `${req.body.city}`,
+        province: `${req.body.province}`,
+        order_deadline: `${req.body.orderDeadline}`,
+        address: `${req.body.address}`,
+        fee: `${req.body.fee}`,
+        fee_waiver: `${req.body.feeWaiver}`
+      };
+      console.log('postQuery: ', postQuery);
+      let scheduleId = req.params.id;
+      connection.query(
+        `UPDATE schedules 
+        SET ?
+        WHERE schedule_id = ?;`,
+        [postQuery, scheduleId],
+        function (err, result) {
+          if (err) {
+            console.log('error in update sched:', err);
+            return res.status(500).send('error:', err);
+          } else {
+            console.log('sched updated: ', result);
+            return res.status(200).send(result);
+          }
+        } 
+      )
+    } else if (hasFee && hasWaiver) {
+      let postQuery = {
+        producer_id_fk_s: `${req.body.producerId}`,
+        schedule_type: `${req.body.type}`,
+        description: `${req.body.description}`,
+        start_date_time: `${req.body.startDateTime}`,
+        end_date_time: `${req.body.endDateTime}`,
+        has_fee: true,
+        has_waiver: true,
+        latitude: `${req.body.latitude}`,
+        longitude: `${req.body.longitude}`,
+        city: `${req.body.city}`,
+        province: `${req.body.province}`,
+        order_deadline: `${req.body.orderDeadline}`,
+        address: `${req.body.address}`,
+        fee: `${req.body.fee}`,
+        fee_waiver: `${req.body.feeWaiver}`
+      };
+      console.log('postQuery: ', postQuery);
+      let scheduleId = req.params.id;
+      connection.query(
+        `UPDATE schedules 
+        SET ?
+        WHERE schedule_id = ?;`,
+        [postQuery, scheduleId],
+        function (err, result) {
+          if (err) {
+            console.log('error in update sched:', err);
+            return res.status(500).send('error:', err);
+          } else {
+            console.log('sched updated: ', result);
+            return res.status(200).send(result);
+          }
+        } 
+      )
+    } else {
+      let postQuery = {
+        producer_id_fk_s: `${req.body.producerId}`,
+        schedule_type: `${req.body.type}`,
+        description: `${req.body.description}`,
+        start_date_time: `${req.body.startDateTime}`,
+        end_date_time: `${req.body.endDateTime}`,
+        has_fee: true,
+        has_waiver: false,
+        latitude: `${req.body.latitude}`,
+        longitude: `${req.body.longitude}`,
+        city: `${req.body.city}`,
+        province: `${req.body.province}`,
+        order_deadline: `${req.body.orderDeadline}`,
+        address: `${req.body.address}`,
+        fee: `${req.body.fee}`,
+        fee_waiver: `${req.body.feeWaiver}`
+      };
+      console.log('postQuery: ', postQuery);
+      let scheduleId = req.params.id;
+      connection.query(
+        `UPDATE schedules 
+        SET ?
+        WHERE schedule_id = ?;`,
+        [postQuery, scheduleId],
+        function (err, result) {
+          if (err) {
+            console.log('error in update sched:', err);
+            return res.status(500).send('error:', err);
+          } else {
+            console.log('sched updated: ', result);
+            return res.status(200).send(result);
+          }
+        } 
+      )
+    }
+    
   },
 
   delete_schedules_id: function (req, res) {
