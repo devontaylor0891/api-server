45679834b7e5569f95019ca9243cd43798e9a03c
devontaylor0891 <devontaylor0891@gmail.com>
2019-09-10 20:13:53 -0600

    Add async method to multi loc notif lambda stuff.

--- src/handlers/lambda/functions.js
+++ src/handlers/lambda/functions.js
@@ -283,44 +283,55 @@ module.exports = {
     },
 
     multiple_location_notification: function (req, res) {
-        // create the new array from the req
-        // call async on each item in the array
-        let locations = req;
-        let schedIds = [];
-        let tempSchedIds;
 
+      let locations = req;
+      let schedIds = [];
+      let tempSchedIds;
+
+      async.waterfall([
+        invokeLambda,
+        setLocNotifSent,
+      ], function(err, result) {
+        console.log('sched Ids completed: ', result);
+      });
+      
+      function invokeLambda(callback) {
         async.forEach(
-            locations,
-            function(location, callback) { //The second argument, `callback`, is the "task callback" for a specific `messageId`
-                // When the lambda fn has returned the payload it will call the "task callback"
-                // This way async knows which items in the collection have finished
-                // first create an array of sched Ids
-                console.log('location for first async: ', location.location);
-                tempSchedIds = location.schedules.map(sched => sched.id);
-                schedIds = schedIds.concat(tempSchedIds);
-                console.log('tempscheds: ', tempSchedIds);
-                console.log('concat schedIds: ', schedIds);
-                // set lambda params
-                var lambda = new AWS.Lambda();
-                var params = {
-                    FunctionName: 'multipleLocationNotificationEmail', /* required */
-                    Payload: JSON.stringify(location)
-                };
-                // send to lambda
-                lambda.invoke(params, function(err, data) {
-                    if (err) { // an error occurred
-                        console.log(err, err.stack);
-                    } else {
-                        console.log('payload: ', data); // successful response
-                        callback;
-                    }    
-                });
-            },
-            function(err) { // 'final' callback - either exits everything when an error occurs, or exits w/o erro after all tasks are complete
-              if (err) return err;
-            }
-        );
+          locations,
+          function(location, callback) { //The second argument, `callback`, is the "task callback" for a specific `messageId`
+              // When the lambda fn has returned the payload it will call the "task callback"
+              // This way async knows which items in the collection have finished
+              // first create an array of sched Ids
+              console.log('location for first async: ', location.location);
+              tempSchedIds = location.schedules.map(sched => sched.id);
+              schedIds = schedIds.concat(tempSchedIds);
+              console.log('tempscheds: ', tempSchedIds);
+              console.log('concat schedIds: ', schedIds);
+              // set lambda params
+              var lambda = new AWS.Lambda();
+              var params = {
+                  FunctionName: 'multipleLocationNotificationEmail', /* required */
+                  Payload: JSON.stringify(location)
+              };
+              // send to lambda
+              lambda.invoke(params, function(err, data) {
+                  if (err) { // an error occurred
+                      console.log(err, err.stack);
+                  } else {
+                      console.log('payload: ', data); // successful response
+                      callback;
+                  }    
+              });
+          },
+          function(err) { // 'final' callback - either exits everything when an error occurs, or exits w/o erro after all tasks are complete
+            if (err) return err;
+          }
+      );
+        // do stuff
+        callback(null, schedIds);
+      };
 
+      function setLocNotifSent(schedIdList, callback) {
         async.forEach(
           schedIds, 
           function(schedId, callback) { //The second argument, `callback`, is the "task callback" for a specific `messageId`
@@ -344,6 +355,71 @@ module.exports = {
               console.log('all scheds changed');
           }
         );
+        // do stuff
+        callback(null, schedIds);
+      };
+
+        // create the new array from the req
+        // call async on each item in the array
+        // let locations = req;
+        // let schedIds = [];
+        // let tempSchedIds;
+
+        // async.forEach(
+        //     locations,
+        //     function(location, callback) { //The second argument, `callback`, is the "task callback" for a specific `messageId`
+        //         // When the lambda fn has returned the payload it will call the "task callback"
+        //         // This way async knows which items in the collection have finished
+        //         // first create an array of sched Ids
+        //         console.log('location for first async: ', location.location);
+        //         tempSchedIds = location.schedules.map(sched => sched.id);
+        //         schedIds = schedIds.concat(tempSchedIds);
+        //         console.log('tempscheds: ', tempSchedIds);
+        //         console.log('concat schedIds: ', schedIds);
+        //         // set lambda params
+        //         var lambda = new AWS.Lambda();
+        //         var params = {
+        //             FunctionName: 'multipleLocationNotificationEmail', /* required */
+        //             Payload: JSON.stringify(location)
+        //         };
+        //         // send to lambda
+        //         lambda.invoke(params, function(err, data) {
+        //             if (err) { // an error occurred
+        //                 console.log(err, err.stack);
+        //             } else {
+        //                 console.log('payload: ', data); // successful response
+        //                 callback;
+        //             }    
+        //         });
+        //     },
+        //     function(err) { // 'final' callback - either exits everything when an error occurs, or exits w/o erro after all tasks are complete
+        //       if (err) return err;
+        //     }
+        // );
+
+        // async.forEach(
+        //   schedIds, 
+        //   function(schedId, callback) { //The second argument, `callback`, is the "task callback" for a specific `messageId`
+        //       // When the db has updated the item it will call the "task callback"
+        //       // This way async knows which items in the collection have finished
+        //       // update the schedule.location_notif_sent to 1 (true)
+        //       console.log('schedId: ', schedId);
+        //       connection.query(
+        //           `SET SQL_SAFE_UPDATES=0;
+        //           UPDATE schedules 
+        //           SET location_notif_sent = 1 
+        //           WHERE schedule_id = ?;
+        //           SET SQL_SAFE_UPDATES=1;`,
+        //           schedId,
+        //           callback
+        //       )
+        //   },
+        //   function(err) { // 'final' callback - either exits everything when an error occurs, or exits w/o erro after all tasks are complete
+        //       if (err) return err;
+        //       // Tell the user about the great success
+        //       console.log('all scheds changed');
+        //   }
+        // );
 
 
     //   console.log('payload received');
