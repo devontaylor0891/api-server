34823b9b9f7a870a5cfb3ded00855807dea081f1
Devon <devontaylor0891@gmail.com>
2020-04-04 11:27:19 -0600

    Redoing get market sched call.

--- src/handlers/markets/{id}.js
+++ src/handlers/markets/{id}.js
@@ -181,17 +181,18 @@ module.exports = {
             function(error, marketSchedulesLocationResult) {
               // let locationData = {};
               if (marketSchedulesLocationResult) {
-                console.log(marketSchedulesLocationResult);
-                // sched.locationData = {
-                //   latitude: row.latitude,
-                //   longitude: row.longitude,
-                //   address: row.address,
-                //   city: row.city,
-                //   province: row.province,
-                //   description: row.description,
-                //   timeframe: row.timeframe,
-                //   locationName: row.location_name
-                // }
+                let row = marketSchedulesLocationResult;
+                // console.log(marketSchedulesLocationResult);
+                sched.locationData = {
+                  latitude: row.latitude,
+                  longitude: row.longitude,
+                  address: row.address,
+                  city: row.city,
+                  province: row.province,
+                  description: row.description,
+                  timeframe: row.timeframe,
+                  locationName: row.location_name
+                };
                 // locationData = marketSchedulesLocationResult.map(function(row) {
                 //   return {
                 //     latitude: row.latitude,
